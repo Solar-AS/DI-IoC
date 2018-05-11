2394acc7fde55cbee9571529bb0743a87bdc23d3
Daniel González García <danielgonnet@gmail.com>
2018-05-11 16:26:24 +0200

    add showcase project and showcase manual configuration

---
+++ src/IoC.Showcase/IoCBasics.cs
@@ -0,0 +1,27 @@
+﻿using DI_IoC.Library;
+using DI_IoC.Library.LowLevel;
+using DI_IoC.Library.LowLevel.LowerLevel;
+using Lamar;
+using NUnit.Framework;
+
+namespace IoC.Showcase
+{
+	[TestFixture]
+	public class IoCBasics
+	{
+		[Test]
+		public void Resolve_ManualConfiguration_Instance()
+		{
+			var container = new Container(cfg =>
+			{
+				cfg.For<TopLevel>().Use<TopLevel>();
+				cfg.For<IFoo>().Use<MaxFoo>();
+				cfg.For<IBar>().Use<InMemoryBar>();
+			});
+
+			var top = container.GetInstance<TopLevel>();
+			
+			Assert.That(top.FooBar(), Is.EqualTo(-5));
+		}
+	}
+}

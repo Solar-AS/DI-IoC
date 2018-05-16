9cef9a763e69484198f6fd049a641ef81069bcc2
Daniel González García <danielgonnet@gmail.com>
2018-05-16 10:17:27 +0200

    factories and delayed resolutions

---
+++ src/IoC.Showcase/Delayed/DelayedTester.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using DI_IoC.Library.LowLevel;
+using DI_IoC.Library.LowLevel.LowerLevel;
+using Lamar;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace IoC.Showcase.Delayed
+{
+	[TestFixture]
+	public class DelayedTester
+	{
+		[Test]
+		public void DelegatedDependency_AutomaticallyConstructed()
+		{
+			var container = new Container(cfg => cfg.AddScoped<IBar>(_ =>
+			{
+				var bar = new InMemoryBar();
+				return bar;
+			}));
+
+			var func = container.GetInstance<Func<IBar>>();
+
+			func().Bar();
+			func().Bar();
+
+			func = container.GetInstance<Func<IBar>>();
+			func().Bar();
+
+			var nested = container.GetNestedContainer();
+			
+			func = nested.GetInstance<Func<IBar>>();
+			func().Bar();
+			func().Bar();
+		}
+
+		[Test]
+		public void LazyDependency_AutomaticallyConstructedOnce()
+		{
+			var container = new Container(cfg => cfg.AddScoped<IBar>(_ =>
+			{
+				var bar = new InMemoryBar();
+				return bar;
+			}));
+
+			var lazy = container.GetInstance<Lazy<IBar>>();
+
+			lazy.Value.Bar();
+			lazy.Value.Bar();
+
+			lazy = container.GetInstance<Lazy<IBar>>();
+			lazy.Value.Bar();
+
+			var nested = container.GetNestedContainer();
+			
+			lazy = nested.GetInstance<Lazy<IBar>>();
+			lazy.Value.Bar();
+			lazy.Value.Bar();
+		}
+	}
+}
---
+++ src/IoC.Showcase/Factories/FactoriesTester.cs
@@ -0,0 +1,25 @@
+﻿using DI_IoC.Library;
+using DI_IoC.Library.LowLevel;
+using DI_IoC.Library.LowLevel.LowerLevel;
+using Lamar;
+using NUnit.Framework;
+
+namespace IoC.Showcase.Factories
+{
+	[TestFixture]
+	public class FactoriesTester
+	{
+		[Test]
+		public void Context_CanBeUser_WhenConfiguringObjects()
+		{
+			var container = new Container(cfg =>
+			{
+				cfg.For<IBar>().Use<InMemoryBar>();
+
+				cfg.For<IFoo>().Use(ctx => new MaxFoo(ctx.GetInstance<IBar>()));
+			});
+
+			Assert.That(container.GetInstance<IFoo>(), Is.InstanceOf<MaxFoo>());
+		}
+	}
+}

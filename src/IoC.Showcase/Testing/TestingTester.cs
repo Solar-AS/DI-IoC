8ae706abc9a23e610441eed86f041e376158e1c6
Daniel González García <danielgonnet@gmail.com>
2018-05-16 15:56:55 +0200

    diagnosing and testing configurations

---
+++ src/IoC.Showcase/Testing/TestingTester.cs
@@ -0,0 +1,55 @@
+﻿using IoC.Showcase.AutoWiring;
+using IoC.Showcase.Lifestyles;
+using Lamar;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace IoC.Showcase.Testing
+{
+	[TestFixture]
+	public class TestingTester
+	{
+		[Test]
+		public void WhatXX_GivesInformation()
+		{
+			var container = new Container(cfg =>
+			{
+				cfg.AddTransient<I1, One>();
+				cfg.AddTransient<I2, Two>();
+				cfg.AddSingleton<Simpleton>();
+
+				cfg.Scan(scan =>
+				{
+					scan.AssemblyContainingType<AutoWiringTester>();
+					scan.ConnectImplementationsToTypesClosing(typeof(ICommandHandler<>));
+					scan.WithDefaultConventions();
+				});
+			});
+			
+			System.Diagnostics.Debug.WriteLine(container.WhatDoIHave());
+
+			System.Diagnostics.Debug.WriteLine(container.WhatDoIHave(typeof(Simpleton)));
+
+			System.Diagnostics.Debug.WriteLine(container.WhatDidIScan());
+		}
+
+		[Test]
+		public void AssertConfigurationIsValid_ChecksForInconsistencies()
+		{
+			new Container(cfg =>
+			{
+				cfg.AddTransient<I1, One>();
+				cfg.AddTransient<I2, Two>();
+				cfg.AddSingleton<Simpleton>();
+			})
+				.AssertConfigurationIsValid();
+
+			new Container(cfg =>
+				{
+					cfg.AddTransient<I1, One>();
+					cfg.AddSingleton<Simpleton>();
+				})
+				.AssertConfigurationIsValid();
+		}
+	}
+}
